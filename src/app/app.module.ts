54e12d31df72e734840ce973db6264b35e46645c
Joakim Isaksson <joakim.isaksson@spinit.se>
2019-08-05 11:33:15 +0200

    added header and form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
